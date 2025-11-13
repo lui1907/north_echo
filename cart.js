028cc9e676b4a2b67c5d2e322f9bdc20ddac42be
lui1907 <ahmetaavcii289@gmail.com>
2025-11-13 11:40:57 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -9,9 +9,29 @@ function saveCart(cart) {
 function updateCartCount() {
     const cart = getCart();
     const count = cart.reduce((sum, item) => sum + item.quantity, 0);
-
     const cartCountEl = document.getElementById("cartCount");
     if (cartCountEl) cartCountEl.textContent = count;
+}
+
+function changeQuantity(name, type) {
+    let cart = getCart();
+    let item = cart.find(i => i.name === name);
+
+    if (!item) return;
+
+    if (type === "increase") {
+        item.quantity += 1;
+    } 
+    if (type === "decrease") {
+        item.quantity -= 1;
+        if (item.quantity <= 0) {
+            cart = cart.filter(i => i.name !== name); // ürünü sil
+        }
+    }
+
+    saveCart(cart);
+    loadCartPage();
+    updateCartCount();
 }
 
 document.addEventListener("DOMContentLoaded", updateCartCount);
