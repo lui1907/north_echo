f11189fcc0098a20942801dfbc33d371ae9f9bc5
lui1907 <ahmetaavcii289@gmail.com>
2025-11-13 10:25:13 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
-document.getElementById('menuBtn').onclick=()=>sideMenu.style.left='0';document.getElementById('closeMenu').onclick=()=>sideMenu.style.left='-250px';
+const sideMenu = document.getElementById("sideMenu");
+const menuBtn = document.getElementById("menuBtn");
+const closeMenu = document.getElementById("closeMenu");
+
+menuBtn.onclick = () => {
+    sideMenu.style.left = "0";
+};
+
+closeMenu.onclick = () => {
+    sideMenu.style.left = "-260px";
+};
