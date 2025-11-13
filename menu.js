bcca9aacc8180bd9906855dfe17995adf0f8dbd0
lui1907 <ahmetaavcii289@gmail.com>
2025-11-13 11:13:18 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,17 @@
-const sideMenu = document.getElementById("sideMenu");
-const menuBtn = document.getElementById("menuBtn");
-const closeMenu = document.getElementById("closeMenu");
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const menuBtn = document.getElementById("menuBtn");
+  const closeBtn = document.getElementById("closeMenu");
+  const sideMenu = document.getElementById("sideMenu");
 
-menuBtn.onclick = () => {
-    sideMenu.style.left = "0px";
-};
+  // MENÜ AÇ
+  menuBtn.addEventListener("click", () => {
+    sideMenu.classList.add("open");
+  });
 
-closeMenu.onclick = () => {
-    sideMenu.style.left = "-260px";
-};
+  // MENÜ KAPAT
+  closeBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("open");
+  });
+
+});
